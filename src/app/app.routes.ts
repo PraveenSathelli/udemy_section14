71fc02e634c74fd15ffa3542df46cafa902fd5db
PraveenSathelli <praveensathelli11@gmail.com>
2026-09-16 23:01:49 +0530

    404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { TasksComponent } from "./tasks/tasks.component";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
 import { NewTaskComponent } from "./tasks/new-task/new-task.component";
+import { NotFoundComponent } from "./shared/not-found/not-found.component";
 
 export const routes: Routes = [
     {
@@ -19,9 +20,13 @@ export const routes: Routes = [
                     component: TasksComponent
                 },
                 {
-                    path:'tasks/new',
-                    component:NewTaskComponent
+                    path: 'tasks/new',
+                    component: NewTaskComponent
                 }
             ]
+    },
+    {
+        path: '**',
+        component: NotFoundComponent
     }
 ];
