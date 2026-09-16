19c5c19bd732e6cf039ae7ad2385ac4b15c929fd
PraveenSathelli <praveensathelli11@gmail.com>
2026-09-16 23:11:13 +0530

    splitted the routes to different files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from "@angular/router";
-import { TasksComponent } from "./tasks/tasks.component";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
-import { NewTaskComponent } from "./tasks/new-task/new-task.component";
 import { NotFoundComponent } from "./shared/not-found/not-found.component";
+import { routes as userRoutes } from './users/users.routes';
 
 export const routes: Routes = [
     {
@@ -13,22 +12,7 @@ export const routes: Routes = [
     {
         path: 'users/:userId', //users/u1,
         component: UserTasksComponent,
-        children:
-            [
-                {
-                    path: '', // <domain>/users/<uid>/tasks
-                    redirectTo: 'tasks',
-                    pathMatch: 'prefix'
-                },
-                {
-                    path: 'tasks', // <domain>/users/<uid>/tasks
-                    component: TasksComponent
-                },
-                {
-                    path: 'tasks/new',
-                    component: NewTaskComponent
-                }
-            ]
+        children: userRoutes
     },
     {
         path: '**',
