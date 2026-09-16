80631610a33c69424a12db755db313d51d098976
PraveenSathelli <praveensathelli11@gmail.com>
2026-09-16 22:57:42 +0530

    dynamic routing using Router

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -2,12 +2,12 @@ import { Component, inject, input, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { TasksService } from '../tasks.service';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [FormsModule,RouterLink],
+  imports: [FormsModule, RouterLink],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css',
 })
@@ -17,6 +17,7 @@ export class NewTaskComponent {
   enteredSummary = signal('');
   enteredDate = signal('');
   private tasksService = inject(TasksService);
+  private router = inject(Router);
 
   onSubmit() {
     this.tasksService.addTask(
@@ -27,5 +28,8 @@ export class NewTaskComponent {
       },
       this.userId()
     );
+    this.router.navigate(['/users', this.userId(), 'tasks'], {
+      replaceUrl: true
+    });
   }
 }
