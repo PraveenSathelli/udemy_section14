c806e892a58848c47cd4c99dc5f0140246faf385
PraveenSathelli <praveensathelli11@gmail.com>
2026-09-17 11:10:11 +0530

    route guards -> canMatch where canActivate:[dummyMatch], will be executed after component matched

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,20 @@
-import { Routes } from "@angular/router";
+import { CanMatchFn, RedirectCommand, Router, Routes } from "@angular/router";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { resolveUserName, UserTasksComponent } from "./users/user-tasks/user-tasks.component";
 import { NotFoundComponent } from "./shared/not-found/not-found.component";
 import { routes as userRoutes } from './users/users.routes';
 import { resolveTitle } from "./tasks/tasks.component";
+import { inject } from "@angular/core";
+
+const dummyMatch: CanMatchFn = (route, segments) => {
+    const router = inject(Router)
+    const shouldgetAccess = Math.random();
+    if(shouldgetAccess < 0.5)
+    {
+        return true
+    }
+ return new RedirectCommand(router.parseUrl('/unauthorized'));
+}
 
 export const routes: Routes = [
     {
@@ -14,6 +25,8 @@ export const routes: Routes = [
         path: 'users/:userId', //users/u1,
         component: UserTasksComponent,
         children: userRoutes,
+        canMatch: [dummyMatch],
+        // canActivate:[dummyMatch],
         data: {
             message: 'Hello! static data'
         },
